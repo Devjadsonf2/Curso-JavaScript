5e6342f3c57faa3eb48b44eded5a51c11fa176ad
Devjadsonf2 <jadsonnn@hotmail.com>
2024-03-20 12:30:18 -0300

    Aprendendo condições

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+
+//console.log ('Oconsole funcionou corretamente!')
+
+var vel = 50
+console.log (`Avelocidade do seu carro é ${vel} Km/h`)
+if (vel >= 60) {
+    console.log ('Você ultrapassou o limite de velocidade.Será multado!')
+}
